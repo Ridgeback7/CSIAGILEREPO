a3d2841909fd9328351f41bddeac8eb927061bf9
Ridgeback7 <abhijit.karche007@gmail.com>
2022-09-01 17:07:54 +0530

    MVC packages pushed

---
+++ CSIHRMAPP/src/main/java/com/csi/controller/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class EmployeeController {
+
+    //Digambar Shinde signIn() at all layers
+
+    //Swapnil Thorat Exception class Generation | saveBulkOfData() at all layers
+
+    //Ayush Agrawal	 filterDataBySal( sal ) at all layers
+
+    //Shraddha Rajigare	Back End Developement	getDataById(id) at all layers
+
+    //Ulkesh Salunkhe	Back End Developement	Make an Enity class |sortDataByName() at all layers
+
+    //Rohit Jadhav	Back End Developement	sortDataBySalary at all layers
+
+    //Nilesh Shirsath	Back End Developement	deleteDataById(id)
+
+    //Rahul Patil	Back End Developement	deleteAllData()
+
+    //Pritam Chavan	Back End Developement	sortDataBySalary()
+
+    //Geeta Desale	Back End Developement	signUp() at all layers
+
+    //Pravin Dighe	Back End Developement	getDataByName(name)
+
+    //Hanuman Bayas	Developement | Testing	"Java Mail API Functionality |
+
+    //Testing funcationality (Dependency on Others )"
+
+    //Shubham Punde	Back End Developement | Database Management	"Validate pom.xml configuration | Create a DataBase| Configure  Application Properties File"
+
+
+}
---
+++ CSIHRMAPP/src/main/java/com/csi/validator/EmployeeValidationHandler.java
@@ -0,0 +1,2 @@
+package com.csi.validator;public class EmployeeValidationHandler {
+}
